466ba6a871bcd1d5063b5a29141c2fedd1880587
SkyEngineerME <guymechatronic@gmail.com>
2022-12-15 20:19:46 +0700

    pbs2.3

---
+++ Problemset2.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int i,x;
+int main(){
+    i = 3;
+    x = 0;
+
+    while(i<69) {
+     cout << "F";
+     i += 1; 
+    }
+    cout << i;
+
+    return 0;
+}
